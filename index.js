6cdcf1660047f5596b11053134db862a54215302
prince-curie <prince.chibuike@gmail.com>
2020-02-15 23:26:33 +0100

    chore: creates a command call crenet to invoke the app

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const { spawn, exec } = require('child_process')
+
+exec('"vue --version"', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+})
