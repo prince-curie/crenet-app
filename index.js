09a45fa6464823ea0dbb18904ffd04985cd461ee
prince-curie <prince.chibuike@gmail.com>
2020-02-16 01:35:03 +0100

    chore: accepts inquiries

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 #!/usr/bin/env node
 
-const { spawn, exec } = require('child_process')
+const { exec, execSync } = require('child_process')
+const inquirer = require('inquirer');
 
-exec('"vue --version"', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
+let installatinAnswer
+
+inquirer.prompt([{
+  type: 'list',
+  name: 'installation',
+  message: 'What form of set up do you need?',
+  choices: ["fullstack adonis app", 'vue and adonis', 'vue app']
+}])
+.then(answers => {
+  installationAnswer = answers.installation;
 })
+
+// exec('vuex --version', (error, stdout, stderr) => {
+//   if (error, stderr) {
+//     execSync('npm install -g @vue/cli', (error, stdout, stderr) => {
+//       if(error || stderr) {
+//         return console.error(`error: ${error} \n stderr: ${stderr}`);
+//       }
+//       console.log(`stdout: ${stdout}`);
+//     })
+//   }
+//   console.log(`stdout: ${stdout}`);
+//   console.error(`stderr: ${stderr}`);
+//   return 
+// })
