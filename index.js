bb1b575dc20bac2428acdf96af922f3d576727bb
prince-curie <prince.chibuike@gmail.com>
2020-02-18 04:46:08 +0100

    chore: set some argument and ensures it runs

--- index.js
+++ index.js
@@ -2,29 +2,68 @@
 
 const { exec, execSync } = require('child_process')
 const inquirer = require('inquirer');
+const date = new Date()
 
-let installatinAnswer
+let installationAnswer
 
-inquirer.prompt([{
-  type: 'list',
-  name: 'installation',
-  message: 'What form of set up do you need?',
-  choices: ["fullstack adonis app", 'vue and adonis', 'vue app']
-}])
-.then(answers => {
-  installationAnswer = answers.installation;
-})
+const argument =  process.argv
 
-// exec('vuex --version', (error, stdout, stderr) => {
-//   if (error, stderr) {
-//     execSync('npm install -g @vue/cli', (error, stdout, stderr) => {
-//       if(error || stderr) {
-//         return console.error(`error: ${error} \n stderr: ${stderr}`);
-//       }
-//       console.log(`stdout: ${stdout}`);
-//     })
-//   }
-//   console.log(`stdout: ${stdout}`);
-//   console.error(`stderr: ${stderr}`);
-//   return 
-// })
+const commands = {
+  version : [ '-v' , '--version' ],
+  help : [ '-h' , '--help' ],
+  create : [ '-n' , '--new' ],
+  make :  [ "-m" , "make" ]
+}
+
+const { version, help, create, make } = commands
+
+if(argument.length > 4 || argument.length == 2 || help.includes(argument[2].toLowerCase())) {
+  return console.log(`
+    Usage: crenet [version | new {project-name}]
+
+    -h, --help      Show this screen
+    -v, --version   show version
+    -n, --new       creates a new project 
+  `)
+
+} else if( version.includes(argument[2].toLowerCase()) ) {
+  const {name, version} = require('./package.json')
+
+  return console.log(`${name} ${version}`)
+} else if ( create.includes(argument[2].toLowerCase()) ) {
+  if (argument[3] == null) {
+    return console.log(`
+      add the project's name
+      crenet new {project-name}
+    `)
+  } else {
+    const projectName = argument[4]
+    inquirer.prompt([{
+      type: 'list',
+      name: 'installation',
+      message: 'What form of set up do you need?',
+      choices: ["fullstack adonis app", 'vue and adonis', 'vue app']
+    }])
+    .then(answers => {
+      installationAnswer = answers.installation;
+      if(installationAnswer == 'vue app') {
+        exec('vue --version', (error, stdout, stderr) => {
+          if (error, stderr) {
+            execSync('npm install -g @vue/cli', (error, stdout, stderr) => {
+              if(error || stderr) {
+                return console.error(`error: ${error} \n stderr: ${stderr}`);
+              }
+              
+              console.log(`stdout: ${stdout}`);
+            })
+          }
+          exec('vue create app')
+          console.log(`stdout: ${stdout}`);
+          console.error(`stderr: ${stderr}`);
+          return 
+        })
+      }
+      
+    })
+  }
+}
