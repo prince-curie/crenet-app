c7f42fb839976dd7bcc4b6b0625c1de70351ec2b
prince-curie <prince.chibuike@gmail.com>
2020-02-23 23:53:30 +0100

    chore: removes the shebang command

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 const { exec, execSync, spawn, spawnSync } = require('child_process')
 const path = require('path')
 const fs = require('fs')
@@ -46,7 +44,7 @@ if(argument.length > 4 || argument.length == 2 || help.includes(argument[2].toLo
       type: 'list',
       name: 'installation',
       message: 'What form of set up do you need?',
-      choices: ["fullstack adonis app", 'vue and adonis app', 'vue app']
+      choices: ["fullstack adonis app", 'vue and adonis app', 'vue app', 'react app', 'react and adonis app']
     }])
       .then(answers => {
         installationAnswer = answers.installation;
@@ -84,6 +82,26 @@ if(argument.length > 4 || argument.length == 2 || help.includes(argument[2].toLo
 
             return
           })
+        } else if (installationAnswer == 'react app') {
+            const proposedProjectPath = `c:/crenet/projects/${date.getFullYear()}/${projectName}`
+            const proposedProjectLocation = `c:/crenet/projects/${date.getFullYear()}`
+            const relativeProjectPath = path.relative(process.cwd(), proposedProjectPath)
+            const isProjectPathAvailable = fs.existsSync(relativeProjectPath)
+            if (isProjectPathAvailable === true) {
+              return console.log('You have created a project by that name.');
+            }
+            fs.mkdirSync(proposedProjectLocation, { recursive: true }, (err) => {
+              if (err) return err;
+            });
+            process.chdir(proposedProjectLocation)
+            console.log(process.cwd());
+
+            const creatingReactApp = spawn('npx', ['create-react-app', `${projectName}`], {
+              stdio: ['inherit', 'inherit', 'inherit'],
+              shell: true
+            })
+            return
+
         } else if (installationAnswer == 'vue and adonis app') {
           exec('vue --version', (error, stdout, stderr) => {
             if (error, stderr) {
@@ -157,6 +175,39 @@ if(argument.length > 4 || argument.length == 2 || help.includes(argument[2].toLo
               shell: true
             })
           })
+        } else if (installationAnswer == 'react and adonis app') {
+            const proposedProjectLocation = `c:/crenet/projects/${date.getFullYear()}/${projectName}`
+            const relativeProjectLocation = path.relative(process.cwd(), proposedProjectLocation)
+            const isProjectLocationAvailable = fs.existsSync(relativeProjectLocation)
+            if (isProjectLocationAvailable === true) {
+              return console.log('You have created a project by that name.');
+            }
+            fs.mkdirSync(proposedProjectLocation, { recursive: true }, (err) => {
+              if (err) return err;
+            });
+            process.chdir(relativeProjectLocation)
+            console.log(process.cwd());
+
+            const reactAppCreatingProcess = spawnSync('npx', ['create-react-app', 'app'], {
+              stdio: ['inherit', 'inherit', 'inherit'],
+              shell: true
+            })
+
+            execSync('adonis --version', (error, stdout, stderr) => {
+              if (error, stderr) {
+                execSync('npm i -g @adonisjs/cli', (error, stdout, stderr) => {
+                  if (error || stderr) {
+                    return console.error(`error: ${error} \n stderr: ${stderr}`);
+                  }
+                })
+              }
+            })
+
+            const adonisApiCreatingProcess = spawn('adonis', ['new', 'api', '--api-only'], {
+              stdio: ['inherit', 'inherit', 'inherit'],
+              shell: true
+            })
+            return 
         }
       })
   }
