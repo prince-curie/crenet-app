e232c8a2b572c03dfef90780ef6a3be7e876a408
prince-curie <prince.chibuike@gmail.com>
2020-02-24 00:41:40 +0100

    chore: creates a a file to handle the help command

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+  return console.log(`
+    Usage: crenet [version | new {project-name}]
+
+    -h, --help      Show this screen
+    -v, --version   show version
+    -n, --new       creates a new project 
+  `)
+}
