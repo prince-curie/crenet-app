38615320ecaedc818b2d214a24d714c662f057fa
prince-curie <prince.chibuike@gmail.com>
2020-02-29 12:15:12 +0100

    chore: throw exceptions when needed

--- utils/install.js
+++ utils/install.js
@@ -5,18 +5,18 @@ const date = new Date()
 
 const InstallLocation = {
   singleFrameworkApp: (stackUse, projectName) => {
-    const proposedPath = `c:/crenet/${stackUse}/${date.getFullYear()}/${projectName}`
-    const proposedLocation = `c:/crenet/${stackUse}/${date.getFullYear()}`
+    const proposedPath = `c:/crenet/${stackUse}s/${date.getFullYear()}/${projectName}`
+    const proposedLocation = `c:/crenet/${stackUse}s/${date.getFullYear()}`
     const relativePath = path.relative(process.cwd(), proposedPath)
     const isPathAvailable = fs.existsSync(relativePath)
     if (isPathAvailable === true) {
-      return console.log(`You have created a ${stackUse} by that name.`);
+      throw Error(`You have created a ${stackUse} by that name.`);
     }
     fs.mkdirSync(proposedLocation, { recursive: true }, (err) => {
-      if (err) return err;
+      if (err) throw Error(err);
     });
     process.chdir(proposedLocation)
-    return console.log(`${ process.cwd() }\\${ projectName }`)
+    return console.log(`${process.cwd()}\\${projectName}`)
   }
 }
 
