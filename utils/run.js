c9387fce758b93d8c89176bfa4656e01311a1abc
prince-curie <prince.chibuike@gmail.com>
2020-05-04 11:25:09 +0100

    chore: cean up  to use execSync and spawn

--- utils/run.js
+++ utils/run.js
@@ -1,32 +1,19 @@
-const { execSync, spawn /*, spawnSync */ } = require('child_process')
+const { execSync, spawn } = require('child_process')
 
 const run = {
   useExecSync: async (argument) => {
-    const installAdonis = await execSync(argument, (error, stdout, stderr) => {
+    return execSync(argument, (error, stdout, stderr) => {
       if (error || stderr) {
         return error || stderr
       }
       return stdout.toString()
     })
-    return installAdonis
   },
 
-  useSpawn: async (argument, options) => {
-    const spawnProcess = await spawn(argument, options, {
-      // stdio: ['inherit', 'inherit', 'inherit'],
-      shell: true
-    })
-    await spawnProcess.on('error', err => console.log(err.message))
-    await spawnProcess.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`)
-    })
-
-    await spawnProcess.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`)
-    })
-    await spawnProcess.on('close', code => console.log('...>>>', code))
-    await spawnProcess.on('exit', code => console.log('...>>>', code))
-  }
+  useSpawn: (argument, options) => spawn(argument, options, {
+    stdio: ['inherit', 'inherit', 'inherit'],
+    shell: true
+  })
 }
 
 module.exports = run
